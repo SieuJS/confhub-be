440a3bddf2c90b46b5e36312ba8b9ee92a1b176b
21120321 <gamefun206@gmail.com>
2024-09-22 14:58:20 +0700

    migrate to digitalOcean

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,27 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
-// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: 'postgres',
-//     logging: false
-// });
-
-const sequelize = new Sequelize(process.env.DB_CONN_STR, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     logging: false,
-});
+    ssl : true,
+    dialectOptions : {
+        ssl: {
+            require: true,
+            ca: fs.readFileSync(path.join(__dirname, '../../certs/ca-certificate.crt')).toString(),
+        }
+    }
+    }
+);
+
+// const sequelize = new Sequelize(process.env.DB_CONN_STR, {
+//     dialect: 'postgres',
+//     logging: false,
+// });
 
 sequelize.authenticate()
     .then(() => console.log('Database connected!'))
